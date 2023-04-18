755972222991fd3ec5f782a64ec31ef20f6f98bb
liv <shadows_withal@fastmail.com>
2023-04-19 00:21:46 +0200

    use readline for... reading lines

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use std::{
-    io::{self, Write},
-    thread,
+use std::{io, sync::Arc};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+    sync::Mutex,
 };
-use tokio::net::TcpStream;
 
 use anyhow::Result;
 use clap::Parser;
+use reedline::{DefaultPrompt, Reedline, Signal};
 
 /// A small DIY IRC client
 #[derive(Parser, Debug)]
@@ -21,56 +23,59 @@ async fn connect_to_server(endpoint: String) -> Result<TcpStream> {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     let args = Args::parse();
+    let stream = connect_to_server(args.server).await?;
+    let (read, mut write) = stream.into_split();
+    let mut line_editor = Reedline::create();
+    let prompt = DefaultPrompt::default();
 
-    println!("Connecting to {}", args.server);
-    let stream = connect_to_server(args.server).await.unwrap();
-    let (read, write) = stream.into_split();
-    print!("> ");
-    io::stdout().flush().unwrap();
-
-    thread::spawn(move || loop {
-        let mut input = String::new();
-
-        match io::stdin().read_line(&mut input) {
-            Ok(_) => {
-                println!("input: {:?}", input);
-                write.try_write(input.as_bytes()).unwrap();
-                ()
+    let arced_read = Arc::new(Mutex::new(read));
+    tokio::spawn(async move {
+        loop {
+            let read = arced_read.clone();
+            let mut buf = [0; 8191];
+            let mut lock = read.lock().await;
+            match lock.read(&mut buf).await.and_then(|n| {
+                match String::from_utf8(Vec::from(&buf[0..n])) {
+                    Ok(n) => Ok(n),
+                    Err(_) => {
+                        println!("ERROR: Malformed response sent from server!");
+                        Err(std::io::Error::new(
+                            io::ErrorKind::InvalidData,
+                            "invalid data",
+                        ))
+                    }
+                }
+            }) {
+                Ok(content) if content.is_empty() => {
+                    println!("Server closed connection, exiting");
+                    std::process::exit(0);
+                }
+                Ok(content) => {
+                    dbg!("{}", content);
+                    ()
+                }
+                Err(_) => {
+                    println!("Internal error!");
+                }
             }
-            Err(error) => println!("Failed reading input: {:?}", error),
         }
     });
 
     loop {
-        let mut buf = [0; 512];
-        read.readable().await.unwrap();
-        match read
-            .try_read(&mut buf)
-            .and_then(|n| match String::from_utf8(Vec::from(&buf[0..n])) {
-                Ok(n) => Ok(n),
-                Err(_) => {
-                    println!("ERROR: Malformed response sent from server!");
-                    Err(std::io::Error::new(
-                        io::ErrorKind::InvalidData,
-                        "invalid data",
-                    ))
-                }
-            }) {
-            Ok(response) if response.is_empty() => {
-                println!("\nServer closed connection. Exiting");
-                std::process::exit(0);
+        let sig = line_editor.read_line(&prompt);
+        match sig {
+            Ok(Signal::Success(buffer)) => {
+                write.write(buffer.as_bytes()).await?;
+                write.flush().await?;
             }
-            Ok(response) => {
-                println!("\n{}", response);
-                print!("> ");
-                io::stdout().flush().unwrap();
+            Ok(Signal::CtrlD) | Ok(Signal::CtrlC) => {
+                break;
             }
-
-            Err(error) => {
-                println!("\nInternal error: {:?}", error);
-            }
+            _ => (),
         }
     }
+
+    Ok(())
 }
